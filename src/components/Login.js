552ef161137fbb28be7ba92f60948a2545ab99fe
Danya Kovan <mr.kovan_2002@mail.ru>
2022-10-26 12:18:48 +0300

    + registration / login components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,21 @@
-
 function Login () {
     return (
-        <div>1</div>
+        <div className="popup popup_type_auth popup_opened">
+            <div className={`popup__content popup__content_type_auth`}>
+                <h1 className="popup__title popup__title_type_auth">Вход</h1>
+                <form className="popup__form popup__form_type_auth">
+                    <div className="popup__input-field">
+                        <input className="popup__input popup__input_content_auth" id="auth-email" name="auth-email" type="email" value='Email' required placeholder="Email" />
+                        <span className="popup__input-error" id="error-name"></span>
+                    </div>
+                    <div className="popup__input-field">
+                        <input className="popup__input popup__input_content_auth" id="auth-password" name="auth-password" type="text" minLength='5' maxLength='15' value='Пароль' required placeholder="Пароль" />
+                        <span className="popup__input-error" id="error-name"></span>
+                    </div>
+                    <button type="submit" className="popup__submit-button popup__submit-button_type_auth">Войти</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom"
+
 function Register () {
     return (
-        <div className="popup popup_type_register popup_opened">
-            <div className={`popup__content popup__content_type_register`}>
-                <h1 className="popup__title popup__title_type_register">Регистрация</h1>
-                <form className="popup__form popup__form_type_register">
+        <div className="popup popup_type_auth popup_opened">
+            <div className={`popup__content popup__content_type_auth`}>
+                <h1 className="popup__title popup__title_type_auth">Регистрация</h1>
+                <form className="popup__form popup__form_type_auth">
                     <div className="popup__input-field">
-                        <input className="popup__input popup__input_content_register" id="registration-email" name="registration-email" type="email" value='Email' required placeholder="Email" />
+                        <input className="popup__input popup__input_content_auth" id="registration-email" name="registration-email" type="email" value='Email' required placeholder="Email" />
                         <span className="popup__input-error" id="error-name"></span>
                     </div>
                     <div className="popup__input-field">
-                        <input className="popup__input popup__input_content_register" id="registration-password" name="registration-password" type="text" value='Пароль' required placeholder="Пароль" />
+                        <input className="popup__input popup__input_content_auth" id="registration-password" name="registration-password" type="text" minLength='5' maxLength='15' value='Пароль' required placeholder="Пароль" />
                         <span className="popup__input-error" id="error-name"></span>
                     </div>
-                    <button type="submit" className="popup__submit-button popup__submit-button_active popup__submit-button_active popup__submit-button_type_register">Зарегистрироваться</button>
+                    <button type="submit" className="popup__submit-button popup__submit-button_type_auth">Зарегистрироваться</button>
                 </form>
-                <p className="popup__form-caption">Уже зарегестрированы? Войти</p>
+                <Link to="sign-in" className="popup__form-caption">Уже зарегестрированы? Войти</Link>
 
             </div>
         </div>
@@ -25,26 +27,7 @@ export default Register
 
 // TODO
 
-//разобраться с классами сабмит кнопки
-
 // разобраться с полями ошибок 
 
-// кнопка должна быть белой
+// убирать футер при окнах регистрации
 
-/*
-<div className={`popup__content popup__content_type_register`}>
-    <button type="button" className={`popup__close-button popup__close-button_type_register`} aria-label="close-popup"></button>
-    <h2 className={`popup__title popup__title_type_register`}>h2</h2>
-    <form className={`popup__form popup__form_type_register`} name="edit-profile" action="#" method="post" //noValidate >
-        <button type="submit" className="popup__submit-button popup__submit-button_active">кнопка</button>
-    </form>
-</div>
-
-*/
-
-/* 
-<form className={`popup__form popup__form_type_${props.name}`} onSubmit={props.onSubmit} name="edit-profile" action="#" method="post" //novalidate >
-{props.children}
-<button type="submit" className="popup__submit-button popup__submit-button_active">{props.button}</button>
-</form>
-*/
