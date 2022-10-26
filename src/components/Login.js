622c2cf235c0c9c560075c4abc46a2e62f0f78bb
Danya Kovan <mr.kovan_2002@mail.ru>
2022-10-26 19:02:59 +0300

    Login / Register components fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,20 @@
 function Login () {
     return (
-        <div className="popup popup_type_auth popup_opened">
-            <div className={`popup__content popup__content_type_auth`}>
-                <h1 className="popup__title popup__title_type_auth">Вход</h1>
-                <form className="popup__form popup__form_type_auth">
-                    <div className="popup__input-field">
-                        <input className="popup__input popup__input_content_auth" id="auth-email" name="auth-email" type="email" value='Email' required placeholder="Email" />
-                        <span className="popup__input-error" id="error-name"></span>
+        <div className="account">
+            
+                <h1 className="account__title">Вход</h1>
+                <form className="account__form">
+                    <div className="account__input-field">
+                        <input className="account__input" id="auth-email" name="auth-email" type="email" value='Email' required placeholder="Email" />
+                        <span className="account__input-error" id="error-name"></span>
                     </div>
-                    <div className="popup__input-field">
-                        <input className="popup__input popup__input_content_auth" id="auth-password" name="auth-password" type="text" minLength='5' maxLength='15' value='Пароль' required placeholder="Пароль" />
-                        <span className="popup__input-error" id="error-name"></span>
+                    <div className="account__input-field">
+                        <input className="account__input" id="auth-password" name="auth-password" type="text" minLength='5' maxLength='15' value='Пароль' required placeholder="Пароль" />
+                        <span className="account__input-error" id="error-name"></span>
                     </div>
-                    <button type="submit" className="popup__submit-button popup__submit-button_type_auth">Войти</button>
+                    <button type="submit" className="account__submit-button account__submit-button_active">Войти</button>
                 </form>
-            </div>
+            
         </div>
     )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,23 +2,22 @@ import { Link } from "react-router-dom"
 
 function Register () {
     return (
-        <div className="popup popup_type_auth popup_opened">
-            <div className={`popup__content popup__content_type_auth`}>
-                <h1 className="popup__title popup__title_type_auth">Регистрация</h1>
-                <form className="popup__form popup__form_type_auth">
-                    <div className="popup__input-field">
-                        <input className="popup__input popup__input_content_auth" id="registration-email" name="registration-email" type="email" value='Email' required placeholder="Email" />
-                        <span className="popup__input-error" id="error-name"></span>
+        <div className="account">
+            <h1 className="popup__title popup__title_type_auth account__title">Регистрация</h1>
+                <form className="account__form">
+                    <div className="account__input-field">
+                        <input className="account__input" id="registration-email" name="registration-email" type="email" value='Email' required placeholder="Email" />
+                        <span className="account__input-error" id="error-name"></span>
                     </div>
-                    <div className="popup__input-field">
-                        <input className="popup__input popup__input_content_auth" id="registration-password" name="registration-password" type="text" minLength='5' maxLength='15' value='Пароль' required placeholder="Пароль" />
-                        <span className="popup__input-error" id="error-name"></span>
+                    <div className="account__input-field">
+                        <input className="account__input" id="registration-password" name="registration-password" type="text" minLength='5' maxLength='15' value='Пароль' required placeholder="Пароль" />
+                        <span className="account__input-error" id="error-name"></span>
                     </div>
-                    <button type="submit" className="popup__submit-button popup__submit-button_type_auth">Зарегистрироваться</button>
+                    <button type="submit" className="account__submit-button account__submit-button_active">Зарегистрироваться</button>
                 </form>
-                <Link to="sign-in" className="popup__form-caption">Уже зарегестрированы? Войти</Link>
+                <Link to="sign-in" className="account__form-caption">Уже зарегестрированы? Войти</Link>
 
-            </div>
+            
         </div>
     )
 }
